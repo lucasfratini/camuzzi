dea519923d0c7e3dd5e990d99299d309b799dba1
lucasfratini <matias_anubis@hotmail.com>
2023-02-27 16:39:59 -0300

    arreglando grillas

--- react/src/components/Cargar.js
+++ react/src/components/Cargar.js
@@ -8,45 +8,54 @@ const Cargar = () => {
         <Grid container spacing={2}>
           <Grid item xs={12} sm={4} pacing={2}>
             <Box
+                height='300%'
                 color='black'
                 bgcolor='red'
                 marginTop={7}
                 border={5}
                 
                 > 
-                <Typography variant="h5" color="initial" align="center">RETIRO DE MEDIDOR</Typography>
+                <Typography 
+                variant="h3" 
+                color="initial" 
+                marginTop={10}
+                align="center">RETIRO DE MEDIDOR</Typography>
                 <Button 
                 variant="contained" 
                 color="primary" 
                 endIcon={<SendIcon />} 
-                size="large">
-                  REETIRO
+                size="large"
+               
+                align="center">
+                  CARGAR
                 </Button>
             </Box> 
           </Grid>
         <Grid item xs={12} sm={4}>
             <Box
+            height='300%'
             color='black'
             bgcolor='yellow'
             marginTop={7}
             border={5}
             
             > 
-            <Typography variant="h5" color="initial"align="center">CORTE DE SERVICIO</Typography>
-            <Button variant="contained" color="primary" endIcon={<SendIcon />}size="large">CORTE
+            <Typography variant="h3" color="initial" align="center" marginTop={10}>CORTE DE SERVICIO</Typography>
+            <Button variant="contained" color="primary" endIcon={<SendIcon />}size="large">CARGAR
             </Button>
             </Box> 
         </Grid>
         <Grid item xs={12} sm={4}>
             <Box
+            height='300%'
             color='black'
             bgcolor='green'
             marginTop={7}
             border={5}
             
             > 
-            <Typography variant="h5"   color="initial" align="center">REAPERTURA DE SERVICIO</Typography>
-            <Button variant="contained" color="primary" endIcon={<SendIcon />}size="large">REAPERTURA
+            <Typography variant="h3"    color="initial" marginTop={10} align="center">REAPERTURA DE SERVICIO</Typography>
+            <Button variant="contained" color="primary" endIcon={<SendIcon />}size="large">CARGAR
             </Button>
             </Box> 
         </Grid>
